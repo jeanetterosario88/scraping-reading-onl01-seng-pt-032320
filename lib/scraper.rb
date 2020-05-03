4353f809f5432a775228729ddaa399f1035084d7
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-05-03 23:19:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,8 @@
-require 'nokogiri'
-require 'open-uri'
+    require 'nokogiri'
+    require 'open-uri'
+     
+    doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+    
+    doc.css(".headline-26OIBN")
 
-html = open("https://flatironschool.com/")
-
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-
-doc.css(".headline-26OIBN").text
+    doc.css(".headline-26OIBN").text
